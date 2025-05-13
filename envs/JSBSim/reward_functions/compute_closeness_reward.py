a1784bbd5354c393b0127a11d8e69cc36d5b230d
hanyaoxuan <2815324301@qq.com>
2025-05-13 23:25:09 +0800

    2v2 距离过远时，让敌机快速接近。

--- envs/JSBSim/reward_functions/compute_closeness_reward.py
+++ envs/JSBSim/reward_functions/compute_closeness_reward.py
@@ -8,29 +8,38 @@ class ComputeClosenessReward(BaseRewardFunction):
     def __init__(self, config):
         super().__init__(config)
         self.optimal_combat_range = getattr(self.config, 'optimal_combat_range')
-
+        self.pre_ave_r = {}
+    def reset(self, task, env):
+        self.pre_ave_r.clear()
+        return super().reset(task, env)
 
     def get_reward(self, task, env, agent_id):
         new_reward = 0
-        ego_range = set()
-        # feature: (north, east, down, vn, ve, vd)
         ego_feature = np.hstack([env.agents[agent_id].get_position(),
                                  env.agents[agent_id].get_velocity()])
+        nums = 0
+        sum_r = 0
         for enm in env.agents[agent_id].enemies:
+            if not enm.is_alive:
+                continue
             enm_feature = np.hstack([enm.get_position(),
                                      enm.get_velocity()])
-            AO, _, R = get_AO_TA_R(ego_feature, enm_feature)
-            if 2000 < R < self.optimal_combat_range:
-                ego_range.add(enm.uid)
-            if R < 2000:
+            _, _, R = get_AO_TA_R(ego_feature, enm_feature)
+            nums += 1
+            sum_r += R
+        if nums == 0:
+            return 0
+        ave_r = sum_r / nums
+        if ave_r > self.optimal_combat_range:
+            if agent_id not in self.pre_ave_r:
+                self.pre_ave_r.update({agent_id: ave_r})
+            pre_ave_r = self.pre_ave_r[agent_id]
+            # 距离变大-20
+            if ave_r - pre_ave_r > 0:
                 new_reward -= 20
-
-        if len(ego_range) > 1:
-            new_reward += 4
-        elif len(ego_range) == 1:
-            new_reward += 2
-        else:
-            new_reward -= 5
+            else:
+                new_reward += 20
+            self.pre_ave_r.update({agent_id: ave_r})
 
         return self._process(new_reward, agent_id)
 
